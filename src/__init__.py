a0035bc78c1ea500228b00a920d98decf30a0aad
sadi <saadamir318@gmail.com>
2025-11-29 20:11:00 +0500

    init src package

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+DDoS Detection System
+A machine learning-based system for detecting and classifying DDoS attacks.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Saad Amir, Asad Shafiq, Usman Khurshid"
